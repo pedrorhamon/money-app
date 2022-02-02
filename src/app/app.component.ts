cb1bcdb5f2ef3ac58ef68951b69a7fb68d3e9e0f
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-02 18:43:20 -0300

    alteraçoes significativas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'money-app';
+  lancamentos = [
+    {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2018',
+  dataPagamento: null, valor: 4.55, pessoa: 'Padaria do Pedro'},
+  {tipo: 'RECEITA', descricao: 'Software', dataVencimento: '30/06/2021',
+  dataPagamento: null, valor: 4.55, pessoa: 'Padaria do Pedro'},
+  {tipo: 'DESPESA', descricao: 'Imposto', dataVencimento: '30/06/2022',
+  dataPagamento: null, valor: 4.55, pessoa: 'Padaria do Pedro'},
+  {tipo: 'DESPESA', descricao: 'Mensalidade', dataVencimento: '30/06/2029',
+  dataPagamento: null, valor: 4.55, pessoa: 'Padaria do Emyli'},
+  {tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '30/06/2030',
+  dataPagamento: null, valor: 4.55, pessoa: 'Academia Fit'},
+  {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
+  dataPagamento: null, valor: 4.55, pessoa: 'Padaria do Pedro'},
+  {tipo: 'DESPESA', descricao: 'Compra de cremes', dataVencimento: '30/06/2016',
+  dataPagamento: null, valor: 4.55, pessoa: 'Padaria do João'},
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
+
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    TableModule
 
   ],
   providers: [],
